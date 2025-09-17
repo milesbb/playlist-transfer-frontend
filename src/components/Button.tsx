a2672e12ba2942a962cc9de8c92bcb1a81aef551
Miles Bailey-Braendgaard <milesbb101@gmail.com>
2025-09-17 20:52:18 +0100

    boilerplate and config updates

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Playlist Transfer</h1>
+      <div className="card p-2">
+        <button>Log in</button>
+        <button>Sign up</button>
+      </div>
+      <p className="read-the-docs">Backup. Migrate. Share.</p>
+    </>
+  );
+}
+
+export default App;
