67711c4a0accbf5968a4b36dd19da892b45f67af
Miles Bailey-Braendgaard <milesbb101@gmail.com>
2025-09-17 21:53:36 +0100

    added gif ordering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import "./App.css";
+import animation from "./cropped_landing_page_dog.gif";
+import title from "./title_light.png";
 
 function App() {
   return (
     <>
-      <div></div>
-      <h1>Playlist Transfer</h1>
-      <div className="card p-2">
-        <button>Log in</button>
-        <button>Sign up</button>
+      <div className="flex flex-col justify-center items-center">
+        <img src={animation} alt="Animated GIF" />
+        <img src={title} alt="Title 'Playlist Transfer'" />
+      </div>
+      <div className="card">
+        <button className="mx-2">Log in</button>
+        <button className="mx-2">Sign up</button>
       </div>
       <p className="read-the-docs">Backup. Migrate. Share.</p>
     </>
