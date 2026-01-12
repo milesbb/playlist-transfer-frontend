d70fa43c531bf59d9e7f9b4154ca1c41880a96b7
Miles Bailey-Braendgaard <milesbb101@gmail.com>
2026-01-12 09:55:07 +0000

    added text to beta dog splash

--- src/pages/BetaDog.tsx
+++ src/pages/BetaDog.tsx
@@ -1,7 +1,35 @@
 import betaDog from "../assets/under-constr-doggie.png";
 
 function BetaDog() {
-  return <img src={betaDog} />;
+  return (
+    <div
+      style={{
+        backgroundColor: "#c3a8bf",
+        borderRadius: "16px",
+        padding: "24px",
+        display: "inline-flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src={betaDog}
+        alt="Under construction dog"
+        style={{
+          maxWidth: "200px",
+          height: "auto",
+        }}
+      />
+      <div
+        style={{
+          marginTop: "12px",
+          fontWeight: "600",
+        }}
+      >
+        Under construction!
+      </div>
+    </div>
+  );
 }
 
 export default BetaDog;
